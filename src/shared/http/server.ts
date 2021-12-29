8bd67106b084c27d1941cd93d161dcb76d4e015c
Jose Junior <juninho@pop-os.localdomain>
2021-12-29 14:58:37 -0300

    configuração do typeorm, e primeira entidade produtos

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
+import 'reflect-metadata';
 import AppError from '@shared/errors/AppError';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
+import '@shared/typeorm/connection';
 import { routes } from './routes';
 
 const app = express();
