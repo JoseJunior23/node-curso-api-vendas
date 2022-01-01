1b09d67a86b7c1969480f811b9b0b60374091d90
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-01-01 16:43:12 -0300

    created forgotpassordrouter for password recovery

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import { productsRouter } from '@modules/products/routes/products.routes';
+import { passwordRouter } from '@modules/users/routes/password.routes';
 import { sessionRouter } from '@modules/users/routes/session.routes';
 import { usersRouter } from '@modules/users/routes/users.routes';
 import { Router } from 'express';
@@ -8,3 +9,4 @@ export const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRouter);
+routes.use('/password', passwordRouter);
