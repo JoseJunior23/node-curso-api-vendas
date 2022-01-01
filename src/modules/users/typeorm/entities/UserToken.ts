19ef895690ca84618dee146bbc35890eba26586d
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-01-01 15:23:38 -0300

    creating entity user_tokens

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('user_tokens')
+export default class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
