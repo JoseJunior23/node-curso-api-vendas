cd645a57181c1f94a9e438a90a9f69b290762d97
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-01-01 16:17:35 -0300

    created services for password recovery

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,36 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepositories';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import { addHours, isAfter } from 'date-fns';
+import { hash } from 'bcryptjs';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+export default class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokensRepository);
+
+    const userToken = await userTokenRepository.findByToken(token);
+    if (!userToken) {
+      throw new AppError('User Token does not exists ');
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+    if (!user) {
+      throw new AppError('User does not exists ');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired');
+    }
+
+    user.password = await hash(password, 8);
+  }
+}
---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,22 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepositories';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+export default class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('User does not exists ');
+    }
+
+    const token = await userTokenRepository.generateToken(user.id);
+  }
+}
