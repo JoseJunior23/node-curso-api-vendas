c452ff4f6321a7961d1fdeec0b684ec9161e0eb2
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-01-03 13:41:19 -0300

    add customer routes

---
+++ src/modules/customers/routes/customers.routes.ts
@@ -0,0 +1,53 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import CustomerController from '../controllers/CustomersController';
+
+export const customersRouter = Router();
+const customersController = new CustomerController();
+
+customersRouter.get('/', customersController.index);
+
+customersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.show,
+);
+
+customersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+    },
+  }),
+  customersController.create,
+);
+
+customersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.update,
+);
+
+customersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.delete,
+);
